733178b04f7be168f7ca9c43a5cc497723eea2f5
jieGit <2857705371@qq.com>
2020-12-18 21:26:06 +0800

    1-02

--- app/router.js
+++ app/router.js
@@ -1,4 +1,8 @@
 module.exports = (app) => {
-    const { router, controller } = app;
-    router.get("/", controller.home.index);
+    const { router } = app;
+    router.get("/login", "sub.home.login");
+    router.get("/signed-in", "sub.home.login");
+
+
+    router.resources("blogs", "/b", "blog");
 }
