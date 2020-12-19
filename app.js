39feaec025982c7d5de830ed99dde697fcce1d5d
jieGit <2857705371@qq.com>
2020-12-19 19:03:01 +0800

    1-07

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 //egg创建好了app之后，如果存在此文件，并且是一个方法，则其会自动运行该方法一次
-// module.exports = app => {
-//     app.axios = require("axios").default;
-// }
+module.exports = app => {
+    app.locals = {
+        globalTitle: "地区数据库"
+    }
+    app.axios = require("axios").default;
+}
 
-// module.exports = {
-//     axios: require("axios").default
-// }
-
-exports.axios = require("axios").default;
