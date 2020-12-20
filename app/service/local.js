cf9d3ad43898a6e5edacd11ebdcc876ba1fdf1c6
jieGit <2857705371@qq.com>
2020-12-20 12:17:21 +0800

    1-12

--- app/service/local.js
+++ app/service/local.js
@@ -1,14 +1,7 @@
 const Service = require("egg").Service;
 module.exports = class extends Service {
     async getProvinces () {
-        const key = "Province";
-        const provinces = await this.app.redis.get(key);
-        if (provinces) {
-            return JSON.parse(provinces);
-        }
-        const resp = await this.app.axios.get(`${this.config.$apiBase}/api/local`);
-        //缓存
-        this.app.redis.set(key, JSON.stringify(resp.data));
-        return resp.data;
+        const provinces = await this.app.redis.get("province");
+        return JSON.parse(provinces);;
     } 
 }
